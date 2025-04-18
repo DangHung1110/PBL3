327cb28c3ff720003acff065e81483fa0cd440b7
DangHung1110 <hung121nkd@gmail.com>
2025-04-18 23:14:41 +0700

    order

---
+++ PBL3/Models/OrderDetail.cs
@@ -0,0 +1,10 @@
+namespace PBL3.Models
+{
+    public class OrderDetail
+    {
+        public int? IDOrder { get; set; }
+        public string IDFood { get; set; }
+        public string IDCustomer { get; set; }
+        public string IDRes { get; set; }
+    }
+}
